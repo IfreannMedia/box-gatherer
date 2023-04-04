412302c9d07d5f18b6c5b6d769f85a17630c56d8
Wario <37343942+IfreannMedia@users.noreply.github.com>
2023-04-04 17:01:21 +0100

    commented jumping code
    will revisit or remove later

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -8,9 +8,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] [Range(0.5f, 10.0f)] private float speed = 5f;
     Vector3 movement;
     Vector3 playerVelocity;
-    [SerializeField] [Range(.5f, 2f)] float jumpHeight = 1.0f;
-    float horizontalRotation, verticalRotation, mouseSensitivity = 5f;
-    float gravityValue = -9.81f;
+    //[SerializeField] [Range(.5f, 2f)] float jumpHeight = 1.0f;
+    //float horizontalRotation, verticalRotation, mouseSensitivity = 5f;
+    //float gravityValue = -9.81f;
     Camera cam;
 
     public void Start()
@@ -35,10 +35,10 @@ public class PlayerMovement : MonoBehaviour
             playerVelocity.y = 0f;
         }
         // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && isGrounded())
-        {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        }
+        //if (Input.GetButtonDown("Jump") && isGrounded())
+        //{
+        //    playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        //}
 
         // face cam forward parallel to ground
         if (horizontalInput != 0 || verticalInput != 0)
@@ -52,7 +52,7 @@ public class PlayerMovement : MonoBehaviour
         }
 
         //if(!isGrounded())
-        playerVelocity.y += gravityValue * Time.deltaTime;
+        //playerVelocity.y += gravityValue * Time.deltaTime;
         charController.Move(playerVelocity * Time.deltaTime);
 
 
