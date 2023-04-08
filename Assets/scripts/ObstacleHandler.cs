4cfda732dddec14b17a8eadcd81c4e95033ba6ba
Wario <37343942+IfreannMedia@users.noreply.github.com>
2023-04-08 18:04:25 +0100

    make knocked boxes blink
    then destroy after certain duration

--- Assets/scripts/ObstacleHandler.cs
+++ Assets/scripts/ObstacleHandler.cs
@@ -6,16 +6,19 @@ using Random = UnityEngine.Random;
 
 public class ObstacleHandler : MonoBehaviour
 {
-    [SerializeField] private AnimationCurve animCurve;
+    [SerializeField] private AnimationCurve knockDownCurve;
+    [SerializeField] [Range(0.5f, 8f)] private float remainActive = 4f;
     private float time = 0.0f;
     private float current = 0.0f;
     private float last = 0.0f;
     float originalY;
+    float animDuration;
 
 
     private void Start()
     {
         originalY = transform.position.y;
+        animDuration = knockDownCurve.keys[knockDownCurve.keys.Length - 1].time;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -28,25 +31,46 @@ public class ObstacleHandler : MonoBehaviour
 
     private IEnumerator handleObstableCollision()
     {
-        // TODO
-        // set destruction timer
-        // Destroy after X time
         GetComponent<BoxCollider>().enabled = false;
         StackManager stackManager = GetComponentInParent<StackManager>();
         stackManager.Remove(GetComponent<BoxPickup>());
         StartCoroutine(MoveAlongCurveY());
         yield return StartCoroutine(ImpulseForceFromCol());
-
         GetComponent<BoxCollider>().enabled = true;
         GetComponent<BoxPickup>().enabled = true;
         GetComponent<Rotate>().enabled = true;
+        yield return StartCoroutine(Blink());
+        Destroy(this.gameObject);
+        
+    }
+
+    private IEnumerator Blink()
+    {
+        float timer = 0.0f;
+        float timeBetweenBlinks = .5f;
+        Renderer rend = GetComponent<Renderer>();
+        while (timer<= remainActive)
+        {
+            // set rend off
+            rend.enabled = false;
+            // wait fraction of second
+            yield return new WaitForSeconds(.05f);
+            // set rend on
+            rend.enabled = true;
+            // leave rendered for timeBetweenBlinks
+            yield return new WaitForSeconds(timeBetweenBlinks);
+            // decrease timeBetweenBlinks until .1f
+            timeBetweenBlinks = timeBetweenBlinks <= .1f ? timeBetweenBlinks : timeBetweenBlinks / 1.5f;
+            // add fraction of second and timeBetweenBlinks to timer
+            timer += .05f + timeBetweenBlinks;
+        }
     }
 
     private IEnumerator ImpulseForceFromCol()
     {
         float duration = 0.0f;
         Vector3 randDir = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1f, 1f));
-        while (duration <= 1.5f)
+        while (duration <= animDuration)
         {
             duration += Time.deltaTime;
             transform.Translate(randDir * Time.deltaTime * 10f);
@@ -59,7 +83,7 @@ public class ObstacleHandler : MonoBehaviour
         bool hasFinishedCurve = false;
         while (!hasFinishedCurve)
         {
-            current = animCurve.Evaluate(time);
+            current = knockDownCurve.Evaluate(time);
             hasFinishedCurve = time > 0.0f && current == last;
 
             transform.position = new Vector3(transform.position.x, originalY + current, transform.position.z);
