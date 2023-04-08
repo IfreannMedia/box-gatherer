6f05493f3cdcf31a9996a473f03b8b479ab7af82
Wario <37343942+IfreannMedia@users.noreply.github.com>
2023-04-08 10:06:14 +0100

    close gap in stack after hit

--- Assets/scripts/StackManager.cs
+++ Assets/scripts/StackManager.cs
@@ -20,10 +20,34 @@ public class StackManager : MonoBehaviour
 
     public void Remove(BoxPickup boxPickup)
     {
-        if (stack.Contains(boxPickup))
+        int index = stack.IndexOf(boxPickup);
+        if (index >= 0)
         {
-            stack.Remove(boxPickup);
+            stack.RemoveAt(index);
             boxPickup.transform.SetParent(null);
+            StartCoroutine(CloseGapInStack());
+        }
+    }
+
+    private IEnumerator CloseGapInStack()
+    {
+        // TODO make stack invincible for X time after hit registered
+        // but not to long to allow skipping obstacles
+        // or maybe just make the trigger colliders on boxes that get shifted down disabled for a brief moment
+        yield return new WaitForSeconds(1f);
+        for (int i = 0; i < stack.Count; i++)
+        {
+            BoxPickup current = stack[i];
+            BoxPickup previous = i > 0 ? stack[i - 1] : null;
+            if (previous != null && current != null)
+            {
+                float verticalDistanceBetween = Vector3.Distance(previous.transform.position, current.transform.position);
+                float distanceToShift = -(verticalDistanceBetween - (previous.transform.localScale.y + ySpacing));
+                if(verticalDistanceBetween > previous.transform.localScale.y + ySpacing)
+                {
+                    current.transform.Translate(new Vector3(0, distanceToShift, 0));
+                }
+            }
         }
     }
 }
