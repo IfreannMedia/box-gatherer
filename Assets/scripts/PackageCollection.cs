cd14d838431e11b554238b62686c37297e244fcd
Wario <37343942+IfreannMedia@users.noreply.github.com>
2023-04-20 19:18:12 +0100

    Update to package collection
    remove box from stack in stackmanager

--- Assets/scripts/PackageCollection.cs
+++ Assets/scripts/PackageCollection.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PackageCollection : MonoBehaviour
 {
-
     public AnimationCurve boxMove;
     [SerializeField] private float speed = .5f;
     [SerializeField] private ScoreManager scoreManager;
@@ -13,13 +12,10 @@ public class PackageCollection : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            // get reference to stack
-            // iterate through stack
-            // for each box we want to move to collection
-            List<BoxPickup> boxPickups = other.GetComponent<StackManager>().getStack();
-            for (int i = boxPickups.Count-1; i > -1; i--)
+            StackManager stackManager = other.GetComponent<StackManager>();
+            List<BoxPickup> boxPickups = stackManager.getStack();
+            for (int i = boxPickups.Count - 1; i > -1; i--)
             {
-                Debug.Log("iterating " + i + " and have box: " + boxPickups[i]);
                 boxPickups[i].transform.SetParent(null);
                 boxPickups[i].GetComponent<BoxCollider>().enabled = false;
                 boxPickups[i].GetComponent<Rotate>().enabled = false;
@@ -27,31 +23,32 @@ public class PackageCollection : MonoBehaviour
                 // TODO add package to collection counter
                 scoreManager.add(boxPickups[i].score);
                 scoreManager.RenderScoreText();
+                stackManager.Remove(boxPickups[i]);
+
             }
-            // which should also disable it's trigger collider
         }
-    }
 
-    IEnumerator moveBoxToCollection(BoxPickup box)
-    {
-        bool hasFinishedCurve = false;
-        float time = 0.0f;
-        Vector3 direction;
-        float distance;
-        Vector3 normalizedDirection;
-        Vector3 translationVector;
-        while (!hasFinishedCurve)
+        IEnumerator moveBoxToCollection(BoxPickup box)
         {
-            direction = transform.position - box.transform.position;
-            distance = direction.magnitude;
-            normalizedDirection = direction / distance;
-            translationVector = normalizedDirection * speed * boxMove.Evaluate(time) * Time.deltaTime;
-            box.transform.Translate(translationVector, Space.World);
+            bool hasFinishedCurve = false;
+            float time = 0.0f;
+            Vector3 direction;
+            float distance;
+            Vector3 normalizedDirection;
+            Vector3 translationVector;
+            while (!hasFinishedCurve)
+            {
+                direction = transform.position - box.transform.position;
+                distance = direction.magnitude;
+                normalizedDirection = direction / distance;
+                translationVector = normalizedDirection * speed * boxMove.Evaluate(time) * Time.deltaTime;
+                box.transform.Translate(translationVector, Space.World);
 
-            time += Time.deltaTime;
-            hasFinishedCurve = direction.magnitude < 0.1f;
-            yield return new WaitForEndOfFrame();
+                time += Time.deltaTime;
+                hasFinishedCurve = direction.magnitude < 0.1f;
+                yield return new WaitForEndOfFrame();
+            }
+            Debug.Log("coroutine finished");
         }
-        Debug.Log("coroutine finished");
     }
 }
